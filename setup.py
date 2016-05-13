4cc17d29a147c2068f869d028a109aef0a83403a
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-05-13 18:08:39 +0900

    version 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="common-ml",
-    version="0.0.2.dev1",
+    version="0.0.2",
     packages=['commonml',
               'commonml.sklearn',
               'commonml.elasticsearch',
