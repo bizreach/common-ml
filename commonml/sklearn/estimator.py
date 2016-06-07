de9c334c6e58c181994cbe130df53608ce26c32b
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-06-07 18:53:33 +0900

    batch size fallback if encountering oom

--- commonml/sklearn/estimator.py
+++ commonml/sklearn/estimator.py
@@ -57,14 +57,14 @@ class ChainerEstimator(BaseEstimator):
         else:
             return self.model(X, y)
 
-    def evaluate(self, X, y, indexes):
+    def evaluate(self, X, y, indexes, batch_size):
         xp = np if self.gpu < 0 else cuda.cupy
         is_spmatrix = isinstance(X, spmatrix)
         data_size = X.shape[0] if is_spmatrix else len(X)
         sum_loss = 0
         has_train = 'train' in inspect.getargspec(self.model.predictor.__call__).args
-        for i in six.moves.range(0, data_size, self.batch_size):
-            end = i + self.batch_size
+        for i in six.moves.range(0, data_size, batch_size):
+            end = i + batch_size
             ids = indexes[i: end if end < data_size else data_size]
             x2 = self.var_x(X[ids], xp)
             y2 = self.var_y(y[ids], xp)
@@ -103,7 +103,7 @@ class ChainerEstimator(BaseEstimator):
                 continue
 
             if self.report > 0 and epoch % self.report == 0:
-                self.evaluate(X, y, indexes)
+                self.evaluate(X, y, indexes, batch_size)
 
             epoch += 1
 
@@ -120,16 +120,27 @@ class ChainerEstimator(BaseEstimator):
         data_size = X.shape[0] if is_spmatrix else len(X)
 
         has_train = 'train' in inspect.getargspec(self.model.predictor.__call__).args
+        batch_size = self.batch_size
         results = None
-        for i in six.moves.range(0, data_size, self.batch_size):
-            end = i + self.batch_size
-            x2 = self.var_x(X[i: end if end < data_size else data_size], xp)
-            pred = self.predict_on_predictor(x2, has_train)
-            if results is None:
-                results = cuda.to_cpu(pred.data)
-            else:
-                results = np.concatenate((results, cuda.to_cpu(pred.data)),
-                                         axis=0)
+        while True:
+            try:
+                for i in six.moves.range(0, data_size, batch_size):
+                    end = i + batch_size
+                    x2 = self.var_x(X[i: end if end < data_size else data_size], xp)
+                    pred = self.predict_on_predictor(x2, has_train)
+                    if results is None:
+                        results = cuda.to_cpu(pred.data)
+                    else:
+                        results = np.concatenate((results, cuda.to_cpu(pred.data)),
+                                                 axis=0)
+            except CUDARuntimeError as e:
+                if 'out of memory' not in e.message:
+                    raise e
+                results = None
+                batch_size = int(batch_size * 0.8)
+                logger.warn(u'Memory shortage. batch_size is changed to %d', batch_size)
+                continue
+            break
 
         return self.model.postpredict_y(results)
 
