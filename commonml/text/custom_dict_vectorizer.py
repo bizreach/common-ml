faf34ae095c0b2a1709280986f6ad7b9986f8e5b
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-11-07 13:52:33 +0900

    add build_custom_vectorizer

--- commonml/text/custom_dict_vectorizer.py
+++ commonml/text/custom_dict_vectorizer.py
@@ -2,15 +2,38 @@
 
 from logging import getLogger
 
+from commonml import es
+from commonml.utils import get_nested_value
 from scipy.sparse.construct import hstack
 from sklearn.base import BaseEstimator
-from sklearn.feature_extraction.text import VectorizerMixin
-from commonml.utils import get_nested_value
+from sklearn.feature_extraction.text import VectorizerMixin, TfidfVectorizer, \
+    CountVectorizer
 
 import numpy as np
 
 
 logger = getLogger('commonml.text.custom_dict_vectorizer')
+
+
+def build_custom_vectorizer(config):
+    vect_rules = []
+    for vect_config in config.values():
+        vect_rule = {}
+        vect_rule['name'] = vect_config.get('name')
+        vect_type = vect_config.pop('type')
+        vect_args = vect_config.get('vectorizer')
+        analyzer_url = vect_config.get('analyzer')
+        if analyzer_url is not None:
+            vect_args['tokenizer'] = es.build_analyzer(analyzer_url)
+        vectorizer = None
+        if vect_type == 'count':
+            vectorizer = CountVectorizer(**vect_args)
+        elif vect_type == 'tfidf':
+            vectorizer = TfidfVectorizer(**vect_args)
+        if vectorizer is not None:
+            vect_rule['vectorizer'] = vectorizer
+            vect_rules.append(vect_rule)
+    return CustomDictVectorizer(vect_rules=vect_rules)
 
 
 def get_nested_str_value(doc, field, default_value=None):
