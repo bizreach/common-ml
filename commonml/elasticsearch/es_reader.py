11ebc2002aad036056a2f3d4f8a7e63ecb75f2ec
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2017-01-27 13:11:34 +0900

    add close

--- commonml/elasticsearch/es_reader.py
+++ commonml/elasticsearch/es_reader.py
@@ -33,14 +33,15 @@ class ElasticsearchReader(object):
         self.request_timeout = request_timeout
         self.es = Elasticsearch(hosts=hosts)
         self.report = report
+        self.scroll_id = None
 
     def __iter__(self):
-        scroll_id = None
+        self.scroll_id = None
         counter = 0
         running = True
-        while(running):
-            try:
-                if scroll_id is None:
+        try:
+            while(running):
+                if self.scroll_id is None:
                     response = self.es.search(index=self.index,
                                               body=self.source,
                                               params={"request_timeout": self.request_timeout,
@@ -48,13 +49,14 @@ class ElasticsearchReader(object):
                                                       "size": self.scroll_size})
                     logger.info(u'{0} docs exist.'.format(response['hits']['total']))
                 else:
-                    response = self.es.scroll(scroll_id=scroll_id,
+                    response = self.es.scroll(scroll_id=self.scroll_id,
                                               params={"request_timeout": self.request_timeout,
                                                       "scroll": self.scroll_time})
                 if len(response['hits']['hits']) == 0:
+                    self.scroll_id = None
                     running = False
                     break
-                scroll_id = response['_scroll_id']
+                self.scroll_id = response['_scroll_id']
                 for hit in response['hits']['hits']:
                     if '_source' in hit:
                         counter += 1
@@ -67,12 +69,21 @@ class ElasticsearchReader(object):
                         if counter % self.report == 0:
                             logger.info(u'%d docs are loaded.', counter)
                         yield hit['_source']
-            except NotFoundError:
-                logger.exception(u'NotFoundError(Loaded %d docs)', counter)
-                break
-            except:
-                logger.exception(u"Failed to load documents from Elasticsearch(Loaded %d docs).",
-                                 counter)
-                break
+        except NotFoundError:
+            logger.exception(u'NotFoundError(Loaded %d docs)', counter)
+        except:
+            logger.exception(u"Failed to load documents from Elasticsearch(Loaded %d docs).",
+                             counter)
 
         logger.info('Loaded %d documents.', counter)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.close()
+
+    def close(self):
+        if self.scroll_id is not None:
+            self.es.clear_scroll(scroll_id=self.scroll_id,
+                                 params={"request_timeout": self.request_timeout})
